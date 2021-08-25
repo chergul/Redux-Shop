69510054d93e072a4028cdfd9f8b4817c2e30005
Ceyda Hergul <ceyda_hergul@hotmail.com>
2021-08-25 19:44:05 -0400

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://chergul:chergul123@cluster0.d3wxp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
